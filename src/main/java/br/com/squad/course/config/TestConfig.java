151a0fc72526c8bc98cee622d738ac71849fb366
Joaomgsa <joaomgsa@gmail.com>
2022-12-14 22:49:33 -0300

    "Seeding Database"

--- src/main/java/br/com/squad/course/config/TestConfig.java
+++ src/main/java/br/com/squad/course/config/TestConfig.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Arrays;
+
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
@@ -19,5 +21,6 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com","988888888","123456");
         User u2 = new User(null,"Alex Green","alex@gmail.com","9777777777","123456");
+        userRepository.saveAll(Arrays.asList(u1,u2));
     }
 }
