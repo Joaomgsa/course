ca96da3efc831d293e0b0b4a835e6c3f276cff89
Joaomgsa <joaomgsa@gmail.com>
2022-12-20 17:00:23 -0300

    Exception Handling - delete.

--- src/main/java/br/com/squad/course/resources/exceptions/ResourceExceptionsHandler.java
+++ src/main/java/br/com/squad/course/resources/exceptions/ResourceExceptionsHandler.java
@@ -1,5 +1,6 @@
 package br.com.squad.course.resources.exceptions;
 
+import br.com.squad.course.services.exceptions.DatabaseException;
 import br.com.squad.course.services.exceptions.ResourceNotfoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,14 @@ public class ResourceExceptionsHandler {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),status.value(),error, e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
--- src/main/java/br/com/squad/course/services/UserService.java
+++ src/main/java/br/com/squad/course/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.squad.course.services;
 
 import br.com.squad.course.entities.User;
 import br.com.squad.course.repositories.UserRepository;
+import br.com.squad.course.services.exceptions.DatabaseException;
 import br.com.squad.course.services.exceptions.ResourceNotfoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotfoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id,User obj){
